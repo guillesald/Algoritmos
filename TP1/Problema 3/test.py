64f3cf3c9b4c13ae437a5c912dfbddf5de5b98e9
Guillermo Ariel Saldaña <141275290+guillesald@users.noreply.github.com>
2023-11-06 14:02:36 -0300

    Update test.py

--- TP1/Problema 3/test.py
+++ TP1/Problema 3/test.py
@@ -1,30 +1,24 @@
 import unittest
 from OrdenamientoExterno import*
+import os
 class TestOrdenamientoExterno(unittest.TestCase):
     
     def test_ordenamiento(self):
         """
-        Compruebo que la cantidad de de datos sea la misma y que los datos
+        Compruebo que el tamaño del archivo sea el mismo y que los datos
         sean de valor creciente.
         """
-        
         generar_numeros_aleatorios("datos_test.txt", 10)
         #generar_numeros_aleatorios("datos_test.txt", 100)  #probar para 100 MB (modificar cantidad de cifras para ahorrar tiempo)
-        numero_lineas_originales = 0
-        with open("datos_test.txt", 'r') as f:
-            for linea in f:
-                numero_lineas_originales += 1
-                
-                
+        tamano_original = os.path.getsize("datos_test.txt")
+   
         ordenar('datos_test.txt')
         
-        numero_lineas_luego_de_ordenar=0
+        tamano_despues_de_ordenar = os.path.getsize('datos_test.txt')
         es_creciente = True
         num_anterior=0
         with open('datos_test.txt', 'r') as f:
             for linea in f:
-                numero_lineas_luego_de_ordenar+=1
-                linea.strip()
                 num_actual=int(linea)
                 if (num_actual>=num_anterior):
                 
@@ -36,7 +30,7 @@ class TestOrdenamientoExterno(unittest.TestCase):
                     break
                     
         self.assertEqual(es_creciente, True)
-        self.assertEqual(numero_lineas_originales, numero_lineas_luego_de_ordenar)
-    
+        self.assertEqual(tamano_original, tamano_despues_de_ordenar)
+
 if __name__ == '__main__':
     unittest.main()
